640119301fa7b3dbbf1494d2e97c73db15085470
Cr1staix <sayhell042@gmail.com>
2024-08-26 12:41:49 +0700

    add new result

--- src/top1/Result.java
+++ src/top1/Result.java
@@ -5,6 +5,9 @@ public class Result {
         int a = 4;
         int b = 6;
         int c =  a + b;
+        if(a < b){
+            c = b - a;
+        }
         System.out.println(c);
     }
 }
