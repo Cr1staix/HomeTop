e8e0ba662905adf2869c2b0b1c093b5a8d6bdc9f
Cr1staix <sayhell042@gmail.com>
2024-08-26 11:58:37 +0700

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("HELLO WORLD");
-        System.out.println("HELLO WORLD");
-        System.out.println("HELLO WORLD");
+        int a = 2;
+        int b = 3;
+        int c = a + b;
+        System.out.println(c);
     }
 }
