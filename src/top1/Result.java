fbd297fcc0b8bec875d38c764a1ded0d2e5ceb85
Cr1staix <sayhell042@gmail.com>
2024-08-26 12:37:02 +0700

    add new result

---
+++ src/top1/Result.java
@@ -0,0 +1,10 @@
+package top1;
+
+public class Result {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 6;
+        int c =  a + b;
+        System.out.println(c);
+    }
+}
